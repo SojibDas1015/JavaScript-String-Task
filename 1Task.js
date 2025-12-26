74b0ecaef674550b0be285b4698dcee34811b81e
Sojib Das <sojibdas1015@gmail.com>
2025-12-26 14:39:05 +0600

    task 1: Count how many times a string has the letter a

---
+++ 1Task.js
@@ -0,0 +1,10 @@
+let sentence = 'Count how many times a string has the letter';
+let cnt = 0;
+for(let letter of sentence)
+{
+    if(letter === 'a')
+    {
+        cnt ++;
+    }
+}
+console.log(cnt);
