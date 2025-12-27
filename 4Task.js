706ca1cb877f42140c589febf946785d8e77d076
Sojib Das <sojibdas1015@gmail.com>
2025-12-27 17:03:01 +0600

    task 4: If a given string has either x, replace x by y. if the given string has X, replace it by Y

---
+++ 4Task.js
@@ -0,0 +1,3 @@
+let sentence = 'If a given string has either x, replace x by y. if the given string has X, replace it by Y';
+console.log(sentence)
+console.log(sentence.replaceAll('x', 'y').replaceAll('X','Y'));
